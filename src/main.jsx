0af0aa5374e6bbb78b6cf659a13d0c02e6de0d18
VectorGits <davidoluremi0@gmail.com>
2024-07-07 18:12:42 +0100

    feat: Update main layout to include Sidebar and MainContent components

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Header from './Header.jsx'
-import Sidebar from './Sidebar.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './Header.jsx';
+import Sidebar from './Sidebar.jsx';
+import MainContent from './MainContent.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <Sidebar />
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex flex-grow">
+        <Sidebar />
+        <MainContent />
+      </div>
+    </div>
   </React.StrictMode>,
-)
+);
