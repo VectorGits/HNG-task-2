89a623a59f6b7de13173efcc3a585304304d02bc
VectorGits <davidoluremi0@gmail.com>
2024-07-07 14:45:10 +0100

    Add Sidebar component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './Header.jsx'
+import Sidebar from './Sidebar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Sidebar />
   </React.StrictMode>,
 )
